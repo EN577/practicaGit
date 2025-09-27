5d920524bacb437b7cc2ac6d7a3908dd68d7b69d
E <chiril.climenco@gmail.com>
2025-09-27 11:28:10 +0200

    Crear formulario

--- practicaGit/Form1.Designer.cs
+++ practicaGit/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            txtTelegrama = new TextBox();
+            cbUrgente = new CheckBox();
+            label2 = new Label();
+            txtPrecio = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.Location = new Point(91, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Texto";
+            // 
+            // txtTelegrama
+            // 
+            txtTelegrama.Location = new Point(91, 54);
+            txtTelegrama.Multiline = true;
+            txtTelegrama.Name = "txtTelegrama";
+            txtTelegrama.Size = new Size(632, 206);
+            txtTelegrama.TabIndex = 1;
+            // 
+            // cbUrgente
+            // 
+            cbUrgente.AutoSize = true;
+            cbUrgente.Location = new Point(91, 289);
+            cbUrgente.Name = "cbUrgente";
+            cbUrgente.Size = new Size(73, 19);
+            cbUrgente.TabIndex = 2;
+            cbUrgente.Text = "Urgente?";
+            cbUrgente.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.Location = new Point(91, 341);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Coste:";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(138, 338);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(100, 23);
+            txtPrecio.TabIndex = 4;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(564, 315);
+            button1.Name = "button1";
+            button1.Size = new Size(159, 66);
+            button1.TabIndex = 5;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(txtPrecio);
+            Controls.Add(label2);
+            Controls.Add(cbUrgente);
+            Controls.Add(txtTelegrama);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox txtTelegrama;
+        private CheckBox cbUrgente;
+        private Label label2;
+        private TextBox txtPrecio;
+        private Button button1;
     }
 }
